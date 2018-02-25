dce76d02e543959debb03b050723175c0e065a77
Snugglez <32309975+Snugglez@users.noreply.github.com>
2018-02-25 11:22:53 -0500

    COLOUR

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Calls out the colors in proxy chat to make things a lot easier(can be edited to 
 
 ![Screenshot](https://i.gyazo.com/813825767eb743a8a5346b6ed51df6ae.png)
 
+NOW IN COLOR!
+
+![Screenshot](https://i.gyazo.com/9e60d1f9a1e75ae9bc91c25b16a3d3b5.png)
+
 Simple? yea simple
 
 Command to toggle off `!tuwang` or just `tuwang` if in proxy chat
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Command = require('command');
 module.exports = function tuwang(dispatch) {
 const command = Command(dispatch)
 let enabled = config.toggle;
+let color = config.noColor;
 let	cid = null
 dispatch.hook('S_LOGIN', 1, (event) => {({cid} = event)});
 //Chat channel example
@@ -19,6 +20,10 @@ command.add('tuwang', () => {
 enabled = !enabled
 command.message(`Tuwang is now ${enabled ? 'enabled' : 'disabled'}.`)})
 
+command.add('tucolor', () => {
+color = !color
+command.message(`No color is now ${color ? 'enabled' : 'disabled'}.`)})
+
 dispatch.hook('S_DUNGEON_EVENT_MESSAGE', 1, (event) => {
 if(!enabled) return;
 if(event.message.includes('700102016'))
@@ -30,7 +35,8 @@ if(!enabled) return;
 if(event.target = cid)
 if(event.skill === 1181680719 || event.skill === 1181680720)
 if(event.templateId === 1001)
-command.message('PINK')
+if(!color) command.message('<font color="#fc76d4">PINK</font>')
+else command.message('PINK')
 })
 
 dispatch.hook('S_ACTION_STAGE', 3, (event) => {
@@ -38,7 +44,8 @@ if(!enabled) return;
 if(event.target = cid)
 if(event.skill === 1181680719 || event.skill === 1181680720)
 if(event.templateId === 1002)
-command.message('GREEN')
+if(!color) command.message('<font color="#76fc78">GREEN</font>')
+else command.message('GREEN')
 })
 
 dispatch.hook('S_ACTION_STAGE', 3, (event) => {
@@ -46,7 +53,8 @@ if(!enabled) return;
 if(event.target = cid)
 if(event.skill === 1181680719 || event.skill === 1181680720)
 if(event.templateId === 1003)
-command.message('RED')
+if(!color) command.message('<font color="#fc7676">RED</font>')
+else command.message('RED')
 })
 
 dispatch.hook('S_ACTION_STAGE', 3, (event) => {
@@ -54,7 +62,8 @@ if(!enabled) return;
 if(event.target = cid)
 if(event.skill === 1181680719 || event.skill === 1181680720)
 if(event.templateId === 1004)
-command.message('BLUE')
+if(!color) command.message('<font color="#7676fc">BLUE</font>')
+else command.message('BLUE')
 })
 }
 
